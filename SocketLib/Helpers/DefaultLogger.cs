23f0595102ac3532f6a354dd1482cb90b72fa575
PKOWORKSTATION\adrim <adrimonge2014@gmail.com>
2025-04-09 18:49:38 -0600

    Helpers Finalizado

--- SocketLib/Helpers/DefaultLogger.cs
+++ SocketLib/Helpers/DefaultLogger.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SocketLib.Interfaces;
 
 namespace SocketLib.Helpers
 {
-    class DefaultLogger
+    // Default implementation of socket logger
+    public class DefaultLogger : ISocketLogger
     {
+        public void Debug(string message)
+        {
+            Console.WriteLine($"[DEBUG] {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+        }
+        public void Info(string message)
+        {
+            Console.WriteLine($"[INFO] {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+        }
+        public void Warning(string message)
+        {
+            Console.WriteLine($"[WARNING] {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine($"[ERROR] {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            Console.WriteLine($"[ERROR] {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+            Console.WriteLine($"Exception: {exception.Message}");
+            Console.WriteLine($"StackTrace: {exception.StackTrace}");
+        }
     }
 }
--- SocketLib/Helpers/SocketFactory.cs
+++ SocketLib/Helpers/SocketFactory.cs
@@ -1,12 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SocketLib.Configuration;
+using SocketLib.Implementation;
+using SocketLib.Interfaces;
 
 namespace SocketLib.Helpers
 {
-    class SocketFactory
+    // Factory for creating socket clients and servers
+    public static class SocketFactory
     {
+        // Create a TCP client
+        public static ISocketClient CreateTcpClient(SocketOptions options = null, ISocketLogger logger = null)
+        {
+            return new TcpSocketClient(options, logger);
+        }
+
+        // Create a TCP server
+        public static ISocketServer CreateTcpServer(SocketOptions options = null, ISocketLogger logger = null)
+        {
+            return new TcpSocketServer(options, logger);
+        }
+
+        // Create a UDP client
+        public static ISocketClient CreateUdpClient(SocketOptions options = null, ISocketLogger logger = null)
+        {
+            // Implementation would be similar to TcpSocketClient but using UdpClient
+            throw new NotImplementedException("UDP client is not implemented yet");
+        }
+
+        // Create a UDP server
+        public static ISocketServer CreateUdpServer(SocketOptions options = null, ISocketLogger logger = null)
+        {
+            // Implementation would be similar to TcpSocketServer but using UdpClient
+            throw new NotImplementedException("UDP server is not implemented yet");
+        }
     }
 }
